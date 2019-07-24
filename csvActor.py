a30fb78dcf5da86a89ec60f2ea1bc7b0053d0cb8
Ashish Garg <mragarg@gmail.com>
2019-07-23 22:07:06 -0400

    Prints category of each line in csv file

--- csvActor.py
+++ csvActor.py
@@ -5,4 +5,6 @@ import csv
 with open('past_nominations.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
 
-        print(csv_reader)
+        # Iterates through each line of the csv file that was read
+        for line in csv_reader:
+            print(line[1]) # Prints the category of award
