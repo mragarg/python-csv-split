06bd917900d83dcffb921a52852019ffbe9ccceb
Ashish Garg <mragarg@gmail.com>
2019-07-23 22:31:58 -0400

    Creates script and returns files with ACTRESS values wanted

---
+++ csvActress.py
@@ -0,0 +1,19 @@
+# Import csv module
+import csv
+
+# Read csv file
+with open('past_nominations.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        # Loops over the first line that contains col names
+        # next(csv_reader)
+
+        # Write new file
+        with open('actress_table.csv', 'w') as new_file:
+            csv_writer = csv.writer(new_file)
+
+            # Iterates through each line of the csv file that was read
+            for line in csv_reader:
+                if line[1] == 'Award' or line[1] == 'ACTRESS IN A LEADING ROLE':
+                    csv_writer.writerow(line)
+                # print(line[1]) # Prints the Award name
